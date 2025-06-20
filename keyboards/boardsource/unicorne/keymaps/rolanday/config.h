1cce4b5f40a0b6f6081d6c2f4b454ddc86b3d95e
MIINII <ft.dawn.o@gmail.com>
2025-06-20 17:47:23 +0900

    testing

--- keyboards/boardsource/unicorne/keymaps/rolanday/config.h
+++ keyboards/boardsource/unicorne/keymaps/rolanday/config.h
@@ -1,74 +1,47 @@
-/*
-This is the C configuration file for the keymap
-
-  Copyright 2022 Mark Stosberg (@markstos)
-  SPDX-License-Identifier: GPL-2.0-or-later
-
-*/
-
 #pragma once
 
-//#define USE_MATRIX_I2C
-
-/* Select hand configuration */
-
+// 마스터 설정
 #define MASTER_LEFT
-// #define MASTER_ RIGHT
-// #define EE_HANDS
 
-//#define SSD1306OLED
-
-
-// By default, when holding a dual-function key shortly after tapping it, the
-// tapped key will begin repeating. This is handy for fast typists when typing
-// words with double letters, such as "happy". If you turn this setting ON, it
-// will be counted as a held modifier instead.
-//#define TAPPING_FORCE_HOLD
-
-// Customized by markstos
+// 탭/홀드
 #define TAPPING_TERM 162
 #define TAPPING_TERM_PER_KEY
-// used for Tapping Term on thumb keys
 #define TAPPING_TERM_THUMB 125
-
-// If you press a dual-role key, press another key, and then release the
-// dual-role key, all within the tapping term, by default the dual-role key
-// will perform its tap action. If the HOLD_ON_OTHER_KEY_PRESS option is
-// enabled, the dual-role key will perform its hold action instead.
+#define PERMISSIVE_HOLD
 #define HOLD_ON_OTHER_KEY_PRESS
 
-// markstos: not sure if these are correct
-// They are intended to beep and flash during flashing
-#define QMK_LED     D5
-#define QMK_SPEAKER C6
+// 원샷 키
+#define ONESHOT_TAP_TOGGLE 3
+#define ONESHOT_TIMEOUT 2000
 
-
-// When enabled, typing a mod-tap plus second within term will register as the mod-combo
-// Ref: https://beta.docs.qmk.fm/using-qmk/software-features/tap_hold#permissive-hold
-#define PERMISSIVE_HOLD
-
-#define COMBO_COUNT 3
-
-// Set the COMBO_TERM so low that I won't type the keys one after each other during normal typing.
-// They would have be held together intentionally to trigger this.
-#define COMBO_TERM 40
-
-// These mostly affect my one-shot Shift key, providing a CapsLock alternative.
-// I want a relatively low timeout, so if I accidentally type "Shift", I can pause just briefly and move on.
-#define ONESHOT_TAP_TOGGLE 3  /* Tapping this number of times holds the key until tapped once again. */
-#define ONESHOT_TIMEOUT 2000  /* Time (in ms) before the one shot key is released */
-
-#define SPLITTHINGHERE true
+// 조이스틱
 #define SPLIT_POINTING_ENABLE
 #define POINTING_DEVICE_RIGHT
-
-#define ANALOG_JOYSTICK_Y_AXIS_PIN GP26
 #define ANALOG_JOYSTICK_X_AXIS_PIN GP27
+#define ANALOG_JOYSTICK_Y_AXIS_PIN GP26
 #define ANALOG_JOYSTICK_SPEED_REGULATOR 13
-#define POINTING_DEVICE_INVERT_Y
 #define POINTING_DEVICE_INVERT_X
-#define POINTING_DEVICE_ROTATION_270	
+#define POINTING_DEVICE_INVERT_Y
+#define POINTING_DEVICE_ROTATION_270
 
-/* Select hand configuration */
-#define OS_DETECTION_DEBUG_ENABLE
-#define OS_DETECTION_KEYBOARD_RESET
+// RGB Matrix
+#define DRIVER_LED_TOTAL 54
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150
+#define RGB_DISABLE_WHEN_USB_SUSPENDED
+#define RGB_MATRIX_KEYPRESSES
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_ALPHAS_MODS
+
+// RGB 효과 허용
+#define ENABLE_RGB_MATRIX_ALPHAS_MODS
+#define ENABLE_RGB_MATRIX_BAND_SAT
+#define ENABLE_RGB_MATRIX_BAND_VAL
+#define ENABLE_RGB_MATRIX_BREATHING
+#define ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
+#define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+
+// OLED
+#define OLED_DRIVER SSD1306
+
+// EEPROM
+#define RP2040_EEPROM_SIZE 4096
